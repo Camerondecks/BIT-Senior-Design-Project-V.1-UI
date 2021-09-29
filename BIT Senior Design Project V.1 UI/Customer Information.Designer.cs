3c797174f05817e84270c41e3d9d72bcfea2d4d2
Camerondecks <89917287+Camerondecks@users.noreply.github.com>
2021-09-28 19:42:47 -0500

    Changed font size and position for "Current Customer Information" heading in Customer Info. Page.

--- BIT Senior Design Project V.1 UI/Customer Information.Designer.cs
+++ BIT Senior Design Project V.1 UI/Customer Information.Designer.cs
@@ -42,10 +42,11 @@ namespace BIT_Senior_Design_Project_V._1_UI
             // lblCustomerInfo
             // 
             this.lblCustomerInfo.AutoSize = true;
-            this.lblCustomerInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCustomerInfo.Location = new System.Drawing.Point(259, 29);
+            this.lblCustomerInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCustomerInfo.Location = new System.Drawing.Point(456, 24);
+            this.lblCustomerInfo.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblCustomerInfo.Name = "lblCustomerInfo";
-            this.lblCustomerInfo.Size = new System.Drawing.Size(283, 24);
+            this.lblCustomerInfo.Size = new System.Drawing.Size(688, 55);
             this.lblCustomerInfo.TabIndex = 1;
             this.lblCustomerInfo.Text = "Current Customer Information";
             // 
@@ -57,40 +58,51 @@ namespace BIT_Senior_Design_Project_V._1_UI
             this.Address,
             this.Phone,
             this.Email});
-            this.dataGridView1.Location = new System.Drawing.Point(178, 203);
+            this.dataGridView1.Location = new System.Drawing.Point(356, 390);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(444, 69);
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.Size = new System.Drawing.Size(888, 133);
             this.dataGridView1.TabIndex = 2;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // CustomerName
             // 
             this.CustomerName.HeaderText = "CustomerInformation";
+            this.CustomerName.MinimumWidth = 10;
             this.CustomerName.Name = "CustomerName";
+            this.CustomerName.Width = 200;
             // 
             // Address
             // 
             this.Address.HeaderText = "Address";
+            this.Address.MinimumWidth = 10;
             this.Address.Name = "Address";
+            this.Address.Width = 200;
             // 
             // Phone
             // 
             this.Phone.HeaderText = "Phone";
+            this.Phone.MinimumWidth = 10;
             this.Phone.Name = "Phone";
+            this.Phone.Width = 200;
             // 
             // Email
             // 
             this.Email.HeaderText = "Email";
+            this.Email.MinimumWidth = 10;
             this.Email.Name = "Email";
+            this.Email.Width = 200;
             // 
             // CustomerInformation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1600, 865);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.lblCustomerInfo);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "CustomerInformation";
             this.Text = "Customer Information";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
